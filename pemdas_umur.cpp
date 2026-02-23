e6f0381a2848f9a2e93cb6157e2afd00cda6a383
bagusplendut-alt <bagusplendut@gmail.com>
2026-02-23 17:23:11 +0700

    menambahkan yang awalnya age sampek 2 jadi 3

--- pemdas_umur.cpp
+++ pemdas_umur.cpp
@@ -9,13 +9,16 @@ int main(){ //program utama
     int age; //variable local
     int age1; //variable local
     int age2; //variable local
+    int age3;
     age = 30;
     age1 = 25;
     age2 = 35;
+    age3 = 60;
 
     cout << "halo world" <<endl;
     cout << 100 << endl;
     cout << age;
     cout << age1;
     cout << age2;
+    cout << age3;
 }   
