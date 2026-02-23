9b74f55be9657e1c0b014463df8070998955c125
bagusplendut-alt <bagusplendut@gmail.com>
2026-02-23 15:58:19 +0700

    first commit

---
+++ pemdas_umur.cpp
@@ -0,0 +1,18 @@
+//contoh library
+#include <iostream>
+using namespace std;
+
+//variable global
+string name;
+
+int main(){ //program utama
+    int age; //variable local
+    int age1; //variable local
+    age = 30;
+    age1 = 25;
+
+    cout << "halo world" <<endl;
+    cout << 99 << endl;
+    cout << age;
+    cout << age1;
+}   
