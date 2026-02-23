66b2909518b3f896c2a843d766a54d5903729f4b
bagusplendut-alt <bagusplendut@gmail.com>
2026-02-23 16:04:21 +0700

    menambahkan age2 = 35

--- pemdas_umur.cpp
+++ pemdas_umur.cpp
@@ -8,11 +8,14 @@ string name;
 int main(){ //program utama
     int age; //variable local
     int age1; //variable local
+    int age2; //variable local
     age = 30;
     age1 = 25;
+    age2 = 35;
 
     cout << "halo world" <<endl;
-    cout << 99 << endl;
+    cout << 100 << endl;
     cout << age;
     cout << age1;
+    cout << age2;
 }   
