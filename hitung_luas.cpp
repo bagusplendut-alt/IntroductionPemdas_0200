7c3d6ab42ed52b38fa4b4d1477937e453e8a7670
bagusplendut-alt <bagusplendut@gmail.com>
2026-02-23 17:28:01 +0700

    mengubah dari mencari luas persegi panjang menajadi luar segitiga

--- hitung_luas.cpp
+++ hitung_luas.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main(){ //mulai langkah
-    int p, l, luas;
+    int a, t, luas;
     
     //langkah 2
-    cout << "masukkan panjang: ";
-    cin >> p;
+    cout << "masukkan alas segitiga: ";
+    cin >> a;
 
     //langkah 3
-    cout << "masukan lebar: ";
-    cin >> l;
+    cout << "masukan tinggi segitiga: ";
+    cin >> t;
 
     //langkah 4
-    luas = p*l;
+    luas = 0.5 * a * t;
 
     //langkah 5
     cout << "hasilnya= " << luas;
